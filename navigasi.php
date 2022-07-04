1713c5254bfeca20b01baffd65106a85e0a2ec94
yandri-syanurdi <yandri.syanurdi12@gmail.com>
2022-07-04 16:59:27 +0700

    initial commit

---
+++ navigasi.php
@@ -0,0 +1,33 @@
+<div id="naviwrap">
+            <div id="navi">
+                <div class="toggleMenu"><i class="glyphicon glyphicon-menu-hamburger"></i> Menu</div>
+                <div style="clear: both"></div>
+                <ul class="naviku">
+                    <li><a href="index.php"><i class="el-icon-home"></i> Beranda</a></li>
+                    <li><a href="tour.php">Tour</a></li>
+                    <li><a href="rental.php">Rental Kendaraan</a></li>
+                    <li class="page_item page-item-5"><a href="tentang-kami.php">Tentang
+                            Kami</a></li>
+                    <li class="page_item page-item-8"><a href="testimonial.php">Testimonial</a>
+                    </li>
+                    <li><a href="konfirmasi.php">Konfirmasi</a>
+                    <li><a href="artikel.php">Artikel Blog</a>
+                        <ul>
+                            <li class="cat-item cat-item-12"><a
+                                    href="adventure.php">Adventure</a>
+                            </li>
+                            <li class="cat-item cat-item-13"><a
+                                    href="backpacker.php">Backpacker</a>
+                            </li>
+                            <li class="cat-item cat-item-11"><a
+                                    href="nature.php">Nature</a>
+                            </li>
+                            <li class="cat-item cat-item-1"><a
+                                    href="uncategorized.php">Uncategorized</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <div style="clear: both"></div>
+            </div>
+        </div>
